c5e6f0149ce3b91d86e685e35f4d299b78f04d96
Thurlow <will.thurlow@l771b8y2-lon.scientificgames.com>
2019-12-10 16:40:49 +0000

    Still broke - collisions don't work properly getting incorrect values (use desmos to check)

--- dec03/manhattan.tcl
+++ dec03/manhattan.tcl
@@ -4,7 +4,8 @@
 global COLLISIONS
 global wire1Points
 global wire2Points
-set fileId [open "input.txt"]
+#set fileId [open "input.txt"]
+set fileId [open "test.txt"]
 seek $fileId 0 start
 
 # Need to use coordinates (x,y). Right will add to x, Left will subtract from x. Up will add to x, down will subtract from x.
@@ -66,38 +67,50 @@ for {set i 0} {$i < [llength $wire2]} {incr i} {
 	
 }
 
-#foreach id [array names wire1Pts] {
-#	puts "$id: $wire2Pts($id)"
-#
-#}
-#set wire1 [split [gets $fileId] ","]
+foreach id [array names wire1Pts] {
+	puts "$id: $wire1Pts($id)"
+}
 
+#set wire1 [split [gets $fileId] ","]
+set w1p1 [list]
+set w1p2 [list]
+set COLLISIONS [list]
+set w2p1 [list]
+set w2p2 [list]
 #puts "[expr [array size wire1Pts] / 2]"
 for {set i 0} {$i < ([expr [array size wire1Pts] / 2]) - 1} {incr i} {
-	set w1p1 "[concat $wire1Pts($i,x) $wire1Pts($i,y)]"
-	set w1p2 "[concat $wire1Pts([expr $i + 1],x) $wire1Pts([expr $i + 1],y)]"
-	puts $w1p1
-	puts $w1p2
-	puts $i
-	for {set j 0} {$i < ([expr [array size wire2Pts] / 2]) - 1} {incr i} {
-		set w2p1 "[concat $wire2Pts($i,x) $wire1Pts($i,y)]"
-		set w2p2 "[concat $wire2Pts([expr $i + 1],x) $wire2Pts([expr $i + 1],y)]"
-		puts $w2p1
-		puts $w2p2	
-		puts $j
-		#if wire 1 y is fixed then we have a horizontal line, so we check the value of w2's y for intersect
-		if {[lindex $w1p1 1] == [lindex $w1p2 1]} {
-		
-			set w1y [lindex $w1p1 1]
+	set w1p1 [list "$wire1Pts($i,x)" "$wire1Pts($i,y)"]
+	set w1p2 [list "$wire1Pts([expr $i + 1],x)" "$wire1Pts([expr $i + 1],y)"]
+	puts "l1p1: [lindex $w1p1 0], [lindex $w1p1 1]"
+	puts "l1p2: [lindex $w1p2 0], [lindex $w1p2 1]"
 	
+	for {set j 0} {$j < ([expr [array size wire2Pts] / 2]) - 1} {incr j} {
+		
+		set w2p1 [list "$wire2Pts($j,x)" "$wire2Pts($j,y)"]
+		set w2p2 [list "$wire2Pts([expr $j + 1],x)" "$wire1Pts([expr $j + 1],y)"]
+#		set w2p1 "[concat $wire2Pts($i,x) $wire1Pts($i,y)]"
+	#	set w2p2 "[concat $wire2Pts([expr $i + 1],x) $wire2Pts([expr $i + 1],y)]"
+	#	puts $w2p1
+	#	puts $w2p2	
+
+		#exit		
+#		puts $j
+		#if wire 1 line a y is fixed then we have a horizontal line, so we check the value of w2's y for intersect
+		if {[lindex $w1p1 1] == [lindex $w1p2 1]} {
+			#if wire 2 is also horizontal then skip as horizontal lines can't collide
+			
+			set w1y [lindex $w1p1 1]
+		#	puts "w1y: $w1y"
 
 		#This is the Y Plane Window we have to pass through with w2
 			set w2minY [expr min([lindex $w2p1 1],[lindex $w2p2 1])]
 			set w2maxY [expr max([lindex $w2p1 1],[lindex $w2p2 1])]
-		
-			if {$w1y > $w2minY && $w1y < $w2maxY} {
+			#puts "maxY: $w2maxY"
+			#puts "minY: $w2minY"
+			if {$w1y >= $w2minY && $w1y <= $w2maxY} {
 	
-				set COLLISIONS($i) [concat [lindex $w2p1 0] $w1y]
+				set COLLISIONS [lappend $COLLISIONS "[expr abs(( 0 - [lindex $w1p1 0]) + ( 0 - $w1y))]"]
+
 			}
 
 		}
@@ -112,9 +125,12 @@ for {set i 0} {$i < ([expr [array size wire1Pts] / 2]) - 1} {incr i} {
 			set w2minX [expr min([lindex $w2p1 0],[lindex $w2p2 0])]
 			set w2maxX [expr max([lindex $w2p1 0],[lindex $w2p2 0])]
 		
-			if {$w1y > $w2minX && $w1y < $w2maxX} {
+			if {$w1x >= $w2minX && $w1x <= $w2maxX} {
 	
-				set COLLISIONS($i) $w1x [concat [lindex $w2p1 1]]
+				
+	
+				set COLLISIONS [lappend $COLLISIONS "[expr abs(( 0 - $w1x) + ( 0 - [lindex $w2p1 1]))]"]
+	
 			}
 
 		}
@@ -125,27 +141,32 @@ for {set i 0} {$i < ([expr [array size wire1Pts] / 2]) - 1} {incr i} {
 		
 }
 
+#puts [expr min($COLLISIONS)]
+	
+#set ans 0
+#
+#
+#
+puts $COLLISIONS
+#foreach id [array names COLLISIONS] {
+	puts "]"
+#	set val [expr abs(( 0 - [lindex $COLLISIONS($id) 0]) + ( 0 - [lindex $COLLISIONS($id) 1]))]
+#	puts $val
+#	
+#	if {$ans == 0 } {
+#		set ans $val
+		#continue;
+	#}
+##	if current answer is bigger than the answer we are looking at 
+#	if {$ans > $val} {
+#		set ans $val;
+#		continue;
+#	}
+#
 
+#}
 
-
-set ans 0
-foreach id [array names COLLISIONS] {
-	#puts $COLLISIONS($id)
-	set val [expr abs([lindex $COLLISIONS($id) 0] + [lindex $COLLISIONS($id) 1])]
-	if {$ans == 0 } {
-		set ans $val
-		continue;
-	}
-
-	if {$ans > $val} {
-		set ans $val;
-		continue;
-	}
-
-
-}
-
-puts $ans
+#puts $ans
 
 
 
