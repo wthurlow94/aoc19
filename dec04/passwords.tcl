7b33e3f4e05961d1517cd2183c44acf0cd04113a
Thurlow <will.thurlow@l771b8y2-lon.scientificgames.com>
2019-12-17 11:37:26 +0000

    Completed

--- dec04/passwords.tcl
+++ dec04/passwords.tcl
@@ -4,27 +4,26 @@
 #set fileId [open "test.txt"]
 #seek $fileId 0 start
 
-set upper 667777
-set lower 667777
-#set upper 847060
-#set lower 372304
+#set upper 677788
 
+#set lower 677788
+set upper 847060
+set lower 372304
+#
 set pointer $lower
 set counter 0
-set lastDupeVal 0
-set lastFailedDupeVal 0
-set hasValidDuplicate "false"
+set validDupeVal 0
 while {$pointer <= $upper } {
 		
-	#reset this flag at each password
-	set lastDupeVal 0
-	set hasValidDuplicate "false"
+
+	set validDupeVal 0
+	set currentDupeCounter 0
+	set failedDupeVal 0
 	#for each character in the pointer
 	for {set i 0} {$i < [string length $pointer]-1} {incr i} {
 		set charX [string index $pointer $i]
 		set charY [string index $pointer [expr $i + 1]]
-
-
+		
 		# For each pair of characters
 		# 1) Check to see if second char decreases - if it does then exit for loop immediately as this password will fail  
 		#
@@ -37,33 +36,41 @@ while {$pointer <= $upper } {
 		#
 		#puts $i
 		#if second char < first char then we exit immediately as the string is invalid
+	#
+	#	puts $i
+	#	puts $charX
+	#	puts $charY
 		if {$charY < $charX} {
-			#puts "FAIL"		 	
-			set hasValidDuplicate "false"
+		 	set validDupeVal 0	
 			break;	
-		}	
-
-
-		if {$charX == $charY} {
-
-			
-			if {$charX == $lastDupeVal} {
-				set hasValidDuplicate "false"
-			} else {
-				set hasValidDuplicate "true"
-				set lastDupeVal $charX
-			}
-
-
-
 		}
+
+		if {$charY > $charX} {
+	       
+			if {$currentDupeCounter == 1} {
+				set validDupeVal $charX
+			}
+			set currentDupeCounter 0
+		}
+
+				
+		if {$charX == $charY} {
+			incr currentDupeCounter 
+			if {$i == [expr [string length $pointer] -2] && $currentDupeCounter == 1} {
+				set validDupeVal $charX
+			}
+		}
+			
 	}
-	
-	if {$hasValidDuplicate == "true"} {
-		puts $pointer
+
+
+	if {$validDupeVal != 0} {
 		incr counter
-	}		
+		puts $pointer
+	}
+			
 	incr pointer
 }
+
 
 puts $counter
